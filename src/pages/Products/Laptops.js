1e4d76fd4d08010ca23f3546e35b3ba0ce326f80
Madootza93 <92996269+Madootza93@users.noreply.github.com>
2024-03-15 19:02:11 +0000

    Update and rename Products.js to Laptops.js

---
+++ src/pages/Products/Laptops.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Laptops.css';
+
+const Laptops = () => {
+  return (
+    <div className="laptops-page">
+      <h1 className="laptops-title">Choose Your Laptop</h1>
+      <div className="laptops-container">
+        <Link to="/macs" className="laptop-section mac" style={{ textDecoration: 'none' }}>
+          <img src='./images/mac.jpg' alt="MacBook" className="laptop-image" />
+          <span className="laptop-title">MacBooks</span>
+        </Link>
+        
+        <Link to="/other" className="laptop-section windows" style={{ textDecoration: 'none' }}>
+          <img id='image' src="./images/other.jpg" alt="Windows Laptop" className="laptop-image" />
+          <span className="laptop-title">Other Laptops</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Laptops;
